fd93021b088f99fc539fa93cba35543d194914e9
Treizer <zescorpion.73@gmail.com>
2026-02-27 20:56:25 -0600

    refactor(api-gateway): improve JWT configuration and secret managment

--- api-gateway/src/main/java/com/treizer/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/treizer/api_gateway/config/SecurityConfig.java
@@ -1,15 +1,38 @@
 package com.treizer.api_gateway.config;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    private final String secretKey;
+
+    public SecurityConfig(
+        @Value("${spring.security.oauth2.resourceserver.jwt.secret-key}")
+        String secretKey
+    ) {
+        this.secretKey = secretKey;
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        SecretKey key = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+        System.out.println("----------JWT SECRET: "+ secretKey);
+
+        return NimbusReactiveJwtDecoder.withSecretKey(key).build();
+    }
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
